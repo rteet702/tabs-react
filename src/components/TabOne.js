39cbac5f9e9cd58c5be426ed2212007d731a544f
Robert Teets <teetsrobert@gmail.com>
2022-09-27 14:55:26 -0600

    3: added simple example callback on tab load

--- src/components/TabOne.js
+++ src/components/TabOne.js
@@ -1,8 +1,11 @@
 const TabOne = () => {
+    const exampleCallback = () => console.log('test')
+
     return (
         <div>
             <p>This is the content of the tab.</p>
             <p>Yeet</p>
+            {exampleCallback()}
         </div>
     )
 }
